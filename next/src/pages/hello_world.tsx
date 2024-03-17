336eaadf0e59962f283ad69c77233291046e010f
takumi_Mac <>
2024-03-17 14:50:11 +0900

    matelialとemotion追加

---
+++ next/src/pages/hello_world.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import SimpleButton from '@/components/SimpleButton'
+
+const HelloWorld: NextPage = () => {
+  const hundleOnClick = () => {
+    console.log("Clicked from hello world!")
+  }
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>content</p>
+      <SimpleButton text={'From HelloWorld'} onClick={hundleOnClick} />
+    </div>
+  )
+}
+
+export default HelloWorld
