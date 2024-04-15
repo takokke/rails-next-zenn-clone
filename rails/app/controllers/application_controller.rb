eeb1c469fc4f41bad8d2707426349a0db92a252a
takumi_Mac <>
2024-04-15 22:54:10 +0900

    rubocop修正

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::API
   # devise_controllerの時のみ、configure_permitted_parametersを実行する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-   protected
+  protected
 
-   def configure_permitted_parameters
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-   end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
